7a137cbb1fe03aef62cefecdefb95a84a6eab32d
Toni Gomis <tonigom@hotmail.com>
2016-03-09 20:16:45 +0100

    oop3

--- Printer/src/Printer.java
+++ Printer/src/Printer.java
@@ -4,16 +4,18 @@
 public class Printer {
     // Constructor: accepta un String amb la representació de tot l'alfabet
     // Hi haurà 27 lletres en total, separades per un espai en blanc.
+
+    private String alphabet;
+    private int contador;
+
     public static void main(String[] args) {
+
 
     }
 
     public Printer(String alphabet) {
-        String data = " #  ##   ## ##  ### ###  ## # # ###  ## # # #   # # ###  #  ##   #  ##   ## ### # # # # # # # # # # ### ###\n" +
-                      "# # # # #   # # #   #   #   # #  #    # # # #   ### # # # # # # # # # # #    #  # # # # # # # # # #   #   #\n" +
-                      "### ##  #   # # ##  ##  # # ###  #    # ##  #   ### # # # # ##  # # ##   #   #  # # # # ###  #   #   #   # \n" +
-                      "# # # # #   # # #   #   # # # #  #  # # # # #   # # # # # # #    ## # #   #  #  # # # # ### # #  #  #      \n" +
-                      "# # ##   ## ##  ### #    ## # # ###  #  # # ### # # # #  #  #     # # # ##   #  ###  #  # # # #  #  ###  # ";
+        this.alphabet = alphabet;
+
     }
 
     // Mètode render: accepta un String amb el text a representar i torna
@@ -21,7 +23,11 @@ public class Printer {
     // Només es consideren les lletres majúscules de la A a la Z. Les
     // minúscules es passen a majúscules, i els altres caràctes es tradueixen
     // a «?» (manco l'espai en blanc).
+
     public String render(String text) {
+        text = text.toUpperCase();
+        String resultat = "";
+        alphabet.substring(108,110);
 
         return null;
     }
